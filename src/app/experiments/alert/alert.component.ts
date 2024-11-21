3774720044c1065233e7a0e7ebf6b7a0cf56d7f2
Chihab Otmani <chihab@gmail.com>
2024-11-21 14:47:57 +0100

    cleanup alert component

--- src/app/experiments/alert/alert.component.ts
+++ src/app/experiments/alert/alert.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 export type MessageType = 'success' | 'error' | 'warning';
 
@@ -7,16 +7,15 @@ export type MessageType = 'success' | 'error' | 'warning';
   selector: 'app-alert',
   imports: [NgClass],
   template: `<div
-      class="alert"
-      [ngClass]="{
+    class="alert"
+    [ngClass]="{
         'alert-success': type === 'success',
         'alert-danger': type === 'error',
         'alert-warning': type === 'warning',
       }"
-    >
-      {{ message }}
-    </div>
-    <div (click)="closeAlert()">X</div>`,
+  >
+    {{ message }}
+  </div>`,
   styles: `
     h2 {
       color: red;
@@ -29,20 +28,4 @@ export class AlertComponent {
 
   @Input()
   type: MessageType = 'success';
-
-  @Input()
-  isError = false;
-
-  @Output()
-  close = new EventEmitter<void>();
-
-  internal = 'This is an internal message';
-
-  constructor() {
-    console.log(this.message);
-  }
-
-  closeAlert() {
-    this.close.emit();
-  }
 }
